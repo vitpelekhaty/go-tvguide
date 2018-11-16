2375314e4d2eed13d6659ec528da83682a69d9bb
vitpelekhaty <vitpelekhaty@gmail.com>
2018-11-16 20:27:34 +0700

    Append license info

--- playlists/xmltv.go
+++ playlists/xmltv.go
@@ -1,3 +1,19 @@
+// IPTV guide viewer
+//
+// Copyright 2018 Vitaly Pelekhaty
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific
+// language governing permissions and limitations under the License.
+
 package playlists
 
 import (
@@ -211,10 +227,10 @@ type XMLTVProgrammeRating struct {
 }
 
 type XMLTVProgrammeStarRating struct {
-	XMLName xml.Name           `xml:"star-rating"`
-	System  string             `xml:"system"`
-	Value   XMLTVValue         `xml:"value"`
-	Icon    XMLTVProgrammeIcon `xml:"icon"`
+	XMLName xml.Name            `xml:"star-rating"`
+	System  string              `xml:"system"`
+	Value   XMLTVProgrammeValue `xml:"value"`
+	Icon    XMLTVProgrammeIcon  `xml:"icon"`
 }
 
 type XMLTVProgrammeReview struct {
